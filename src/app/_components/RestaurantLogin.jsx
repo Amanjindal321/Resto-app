25a381d5ad7871faadb484bf37929b26338722d6
Aman <jindalaman405@gmail.com>
2024-12-03 19:01:55 +0530

    fourth commit

--- src/app/_components/RestaurantLogin.jsx
+++ src/app/_components/RestaurantLogin.jsx
@@ -19,7 +19,7 @@ const RestaurantLogin=()=>{
         }
         let response= await fetch("http://localhost:3000/api/restaurant",{
             method:'POST',
-           // mode: 'no-cors', //
+            mode: 'no-cors', //
             headers:{
                 'Content-Type': 'application/json',
             },
