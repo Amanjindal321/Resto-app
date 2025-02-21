c2014427a76cf6db5d3224088378c3182025dfcc
Aman <jindalaman405@gmail.com>
2025-02-21 19:11:16 +0530

    commit

--- src/app/lib/foodsModel.js
+++ src/app/lib/foodsModel.js
@@ -1,11 +1,24 @@
-const { default: mongoose } = require("mongoose");
+// const { default: mongoose } = require("mongoose");
 
-const foodModel=new mongoose.Schema({
-    name:String,
-    price:String,
+// const foodModel=new mongoose.Schema({
+//     name:String,
+//     price:String,
+//     path:String,
+//     description:String,
+//     resto_id:mongoose.Schema.Types.ObjectId
+// });
+
+// export const foodSchema=mongoose.models.foods || mongoose.model("foods",foodModel)
+
+
+import mongoose from "mongoose";
+
+const foodSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
     path:String,
     description:String,
-    resto_id:mongoose.Schema.Types.ObjectId
+    resto_id: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
 });
 
-export const foodSchema=mongoose.models.foods || mongoose.model("foods",foodModel)
+export default mongoose.models.foods || mongoose.model("foods", foodSchema);
